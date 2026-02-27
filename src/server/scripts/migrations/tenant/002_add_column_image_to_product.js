44a840f29d57f98baddc0ecbac226c6ab44f499d
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2026-02-27 15:38:44 -0300

    fix migration and db creation

--- src/server/scripts/migrations/tenant/002_add_column_image_to_product.js
+++ src/server/scripts/migrations/tenant/002_add_column_image_to_product.js
@@ -1,5 +1,5 @@
 export const description = 'Agregar columna imagen_url a Producto';
 
 export function up(db) {
-  db.exec(`ALTER TABLE Producto ADD COLUMN imagen_url TEXT`);
+  // db.exec(`ALTER TABLE Producto ADD COLUMN imagen_url TEXT`);
 }
